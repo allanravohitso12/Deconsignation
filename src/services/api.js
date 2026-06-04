a9b12a192bef8cb0ec2b43c2a7b29652312115a7
allanravohitso12 <allanravohitso@gmail.com>
2026-06-04 22:07:01 +0300

    fixed

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://deconsignation-back.onrender.com/api",
+  baseURL: "https://deconsignation-back-1.onrender.com/api",
 });
 
 export default API;
